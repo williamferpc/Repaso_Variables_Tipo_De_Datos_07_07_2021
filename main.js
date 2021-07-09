819b343ab736b3304db85b6127affe2f09fae72e
williamferpc <willi_9212@hotmail.com>
2021-07-09 18:46:39 -0500

    CAMBIOS 07_07_2021

---
+++ main.js
@@ -0,0 +1,14 @@
+nombreCompleto = `WILLIAM FERNANDO PEÑA CORZO`;
+miEdad = 28;
+// altura = 1.43;
+soyUnProfesor = false;
+
+
+console.log(nombreCompleto, typeof(nombreCompleto));
+console.log(miEdad, typeof(miEdad));
+// console.log(altura, typeof(altura));
+console.log(soyUnProfesor, typeof(soyUnProfesor));
+
+
+// alert()
+// document.write();
